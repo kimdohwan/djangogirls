8dfa6eee79589818e5bcddeb69a947a70d485d1f
dohwan <dosio0102@gmail.com>
2018-06-04 15:08:34 +0900

    post_list에서 동적으로 글목록 문자열을 만들어 리턴해보기

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,7 +1,10 @@
 import os
 
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template.loader import render_to_string
+
+from blog.models import Post
 
 
 def post_list(request):
@@ -16,7 +19,31 @@ def post_list(request):
     # blog__post_list_file_path = os.path.join(templates_file_path, 'blog', 'post_list.html')
     # html = open(blog__post_list_file_path, 'rt').read()
 
-    # 경로에 해당하는 문자열을 로드해줌
-    html = render_to_string('blog/post_list.html')
-    # 가져온 문자열을 돌려주기
-    return HttpResponse(html)
+    # # 경로에 해당하는 문자열을 로드해줌
+    # html = render_to_string('blog/post_list.html')
+    # # 가져온 문자열을 돌려주기
+    # return HttpResponse(html)
+    # 위 두 과정을 한줄로 바꿔줄 수 있다
+    # return render(request, 'blog/post_list.html')
+
+    # Post인스턴스에서 title 속성에 접근가능
+    # HttpResponse에
+    # 글 목록
+    # - 글제목..
+    # - 글제목..
+    # ...
+    # 위 텍스트를 넣어서 리턴
+    posts = Post.objects.all()
+    print(posts)
+
+    result = '글 목록<br>'
+    for post in Post.objects.all():
+        result += '{}<br>'.format(post.title)
+
+    # po = ['글 목록', ]
+    # for post in Post.objects.all():
+    #     po.append(f'<br> - {post.title}')
+
+    # result = '\n'.join([post for post in Post.objects.all()])
+
+    return HttpResponse(result)
